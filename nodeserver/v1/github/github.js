28e355b0e0b3e8dc86d3d063c4ef08b4442b6270
russjohnson09@gmail.com <russjohnson09@gmail.com>
2017-03-04 11:46:31 -0500

    added link to github account

--- nodeserver/v1/github/github.js
+++ nodeserver/v1/github/github.js
@@ -10,6 +10,9 @@ var bcrypt = require('bcrypt');
 const mongoose = require("mongoose"); //http://mongoosejs.com/docs/index.html
 const request = require('request');
 
+const ObjectID = mongodb.ObjectID;
+
+
 module.exports = function (opts) {
 
     var winston = opts.winston;
@@ -24,14 +27,7 @@ module.exports = function (opts) {
 
     self.router = router;
 
-
-
-    const Schema = mongoose.Schema;
-
-    const User = mongoose.model('User', Schema({
-        access_token_github: String,
-        id_github: Number
-    }));
+    const User = opts.db.collection('user');
 
 
     //mongodb connection
@@ -41,7 +37,7 @@ module.exports = function (opts) {
     const MongoClient = mongodb.MongoClient
 
 
-    const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017';
+    const MONGO_URI = process.env.MONGO_URI;
     const GITHUB_OAUTH_URL = 'https://github.com/login/oauth/authorize';
     const GITHUB_ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token';
     const GITHUB_API_URL = 'https://api.github.com';
@@ -92,140 +88,160 @@ module.exports = function (opts) {
     //setup routes
     (function () {
 
-        router.get('/github/profile', function (req, res) {
+        router.get('/github/user', function (req, ExpressRes) {
             var sess = req.session;
             var user = req.user;
-            res.json(user);
-        });
-
-        router.get('/github/link', function (req, res) {
-            var sess = req.session;
-            winston.debug(sess)
-            winston.debug(req.query);
-            if (req.query.code && req.query.state
-                && sess.github_oauth_state === req.query.state) {
-                winston.debug('has code');
-                var code = req.query.code;
-
-                addUpdateGithubUser(sess,code, req.query.state, function (err, user) {
-                    if (err) {
-                        return res.end();
-
-                    }
-                    if (user) {
-                        sess.user = user;
-                        return res.redirect('/profile');
-                    }
-                })
-
+            if (!user.access_token_github) {
+                return res.redirect('/github/link');
             }
-            else if (req.query.code) {
-                res.redirect(400, '/');
-                res.json({ message: "bad request" });
-            }
-            else {
-                sess.github_oauth_state = getToken();
-                var github_auth_redirect = getGithubAuthRedirect();
-                github_auth_redirect += '&state=' + sess.github_oauth_state;
-                res.redirect(github_auth_redirect);
-            }
-        });
-    })()
-
-
-    function getToken() {
-        return crypto.randomBytes(64).toString('hex');
-    }
-
-    function getGithubAuthRedirect() {
-        var token = getToken();
-        var url = GITHUB_OAUTH_URL + '?scope=user,repo&client_id='
-            + process.env.GITHUB_ID;
-        return url;
-    }
-
-
-
-    function addUpdateGithubUser(sess,code, state, callback) {
-        request.post({
-            headers: {
-                'content-type': 'application/json',
-                'Accept': 'application/json'
-            },
-            url: GITHUB_ACCESS_TOKEN_URL,
-            body: JSON.stringify({
-                client_id: process.env.GITHUB_ID,
-                client_secret: process.env.GITHUB_SECRET,
-                code: code,
-                state: state
-            })
-        }, function (error, res, body) {
-            winston.debug(res.headers);
-            winston.debug(res.statusCode);
-            if (res.statusCode !== 200) {
-                winston.error('unexected statusCode ' + res.statusCode);
-            }
-            else {
+            request.get({
+                headers: {
+                    'content-type': 'application/json',
+                    'Accept': 'application/json',
+                    'User-Agent': CODERUSS_USER_AGENT,
+                    'Authorization': 'token ' + user.access_token_github,
+                },
+                url: GITHUB_API_URL + '/user',
+            }, function (err, res, body) {
+                winston.debug(res.statusCode);
                 winston.debug(body);
-                var data = JSON.parse(body);
-                var access_token_github = data.access_token;
-                if (!access_token_github) {
-                    winston.error('failed to get access_token');
-                }
-                else {
-                    request.get({
-                        headers: {
-                            'content-type': 'application/json',
-                            'Accept': 'application/json',
-                            'User-Agent': CODERUSS_USER_AGENT,
-                            'Authorization': 'token ' + access_token_github,
-                        },
-                        url: GITHUB_API_URL + '/user',
-                    }, function (err, res, body) {
-                        winston.debug(res.statusCode);
-                        winston.debug(body);
 
+                ExpressRes.status(res.statusCode);
+                return ExpressRes.send(body);
+            });
+        });
+
+            router.get('/github/link', function (req, res) {
+                var sess = req.session;
+
+                if (req.query.code && req.query.state
+                    && sess.github_oauth_state === req.query.state) {
+                    winston.info('has code');
+                    var code = req.query.code;
+
+                    addUpdateGithubUser(req.user, code, req.query.state, function (err, user) {
                         if (err) {
                             winston.error(err);
-                            return callback();
-                        }
-                        var data = JSON.parse(body);
-                        if (!data.id) {
-                            winston.error('could not link github account');
-                            return callback();
-                        }
-                        User.findOne(
-                            {
-                                _id: sess.user._id,
-                            }, function (err, user) {
-                                if (user) {
-                                    winston.debug('user already exists');
-                                    return callback(null, user);
-                                }
-                                else {
-                                    user = new User();
-                                    user.access_token_github = access_token_github;
-                                    user.id_github = data.id;
-                                    user.save(function (err, product, numAffected) {
-                                        if (err) {
-                                            winston.error(err);
-                                            return callback();
 
-                                        }
-                                        winston.debug('user saved/updated', numAffected);
-                                        return callback(null, user);
-
-                                    })
-                                }
-                            });
+                        }
+                        res.redirect('/profile');
+                        return res.end();
 
                     })
 
                 }
-            }
-        })
+                else if (req.query.code) {
+                    res.redirect(400, '/');
+                    return res.json({ message: "bad request" });
+                }
+                else {
+                    sess.github_oauth_state = getToken();
+                    var github_auth_redirect = getGithubAuthRedirect();
+                    github_auth_redirect += '&state=' + sess.github_oauth_state;
+                    return res.redirect(github_auth_redirect);
+                }
+            });
+        })()
 
-    }
 
-    return self;
-};
+        function getToken() {
+            return crypto.randomBytes(64).toString('hex');
+        }
+
+        function getGithubAuthRedirect() {
+            var token = getToken();
+            var url = GITHUB_OAUTH_URL + '?scope=user,repo&client_id='
+                + process.env.GITHUB_ID;
+            return url;
+        }
+
+
+
+        function addUpdateGithubUser(user, code, state, callback) {
+            request.post({
+                headers: {
+                    'content-type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                url: GITHUB_ACCESS_TOKEN_URL,
+                body: JSON.stringify({
+                    client_id: process.env.GITHUB_ID,
+                    client_secret: process.env.GITHUB_SECRET,
+                    code: code,
+                    state: state
+                })
+            }, function (error, res, body) {
+                winston.debug(res.headers);
+                winston.debug(res.statusCode);
+                if (res.statusCode !== 200) {
+                    winston.error('unexected statusCode ' + res.statusCode);
+                }
+                else {
+                    winston.debug(body);
+                    var data = JSON.parse(body);
+                    var access_token_github = data.access_token;
+                    if (!access_token_github) {
+                        winston.error('failed to get access_token');
+                    }
+                    else {
+                        request.get({
+                            headers: {
+                                'content-type': 'application/json',
+                                'Accept': 'application/json',
+                                'User-Agent': CODERUSS_USER_AGENT,
+                                'Authorization': 'token ' + access_token_github,
+                            },
+                            url: GITHUB_API_URL + '/user',
+                        }, function (err, res, body) {
+                            winston.debug(res.statusCode);
+                            winston.debug(body);
+
+                            if (err) {
+                                winston.error(err);
+                                return callback();
+                            }
+                            var data = JSON.parse(body);
+                            if (!data.id) {
+                                winston.error('could not link github account');
+                                return callback();
+                            }
+                            winston.info(user._id);
+                            User.findOne({ _id: user._id }, function (err, result) {
+                                if (err) {
+                                    winston.error(err);
+                                }
+                                if (!result) {
+                                    winston.error('could not find user', user)
+                                    return callback();
+                                }
+                                else {
+                                    winston.info(result);
+
+                                    User.updateOne({ _id: user._id },
+                                        {
+                                            $set:
+                                            {
+                                                access_token_github: access_token_github
+                                            }
+                                        }, function (error, result) {
+                                            if (error) {
+                                                winston.error(error);
+                                                return callback();
+                                            }
+                                            winston.info(result.result);
+                                            winston.info(result.upsertedId);
+                                            return callback();
+                                        });
+                                }
+                            });
+
+                        })
+
+                    }
+                }
+            })
+
+        }
+
+        return self;
+    };

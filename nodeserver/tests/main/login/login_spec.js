e1c93a572354a994e3a12a04a5dc6cfc3f403820
Russ Johnson <russjohnson09@gmail.com>
2017-03-24 12:16:26 +0000

    adding new requestpasswordreset endpoint

--- nodeserver/tests/main/login/login_spec.js
+++ nodeserver/tests/main/login/login_spec.js
@@ -1,22 +1,35 @@
 var request = require('request');
 var expect = require("chai").expect;
+var winston = require('winston');
 
 
+const CONSOLE_LOG_LEVEL = process.env.CONSOLE_LOG_LEVEL || 'info';
 
-describe("login endpoints >>", function () {
+winston.loggers.add('testlogger', {
+  transports: [
+    new(winston.transports.Console)({
+      level: CONSOLE_LOG_LEVEL
+    }),
+  ]
+});
+
+var logger = winston.loggers.get('testlogger');
+
+
+describe("login endpoints >>", function() {
 
   var baseurl = "http://localhost:3000";
   var loginurl = baseurl + '/v1/login';
-  describe('/v1/login', function () {
-    it("successfully login with admin:admin", function (done) {
+  describe('/v1/login', function() {
+    it("successfully login with admin@foo.com:admin@foo.com", function(done) {
       request({
         method: "POST",
         json: {
-          "username": "admin",
-          "password": "admin"
+          "username": "admin@foo.com",
+          "password": "admin@foo.com"
         },
         uri: loginurl
-      }, function (error, response, body) {
+      }, function(error, response, body) {
         expect(error).to.be.equal(null);
         expect(response.statusCode).to.equal(201);
         expect(response.headers['content-type']).to.be.equal('application/json; charset=utf-8');
@@ -28,8 +41,8 @@ describe("login endpoints >>", function () {
   });
 
   var uri = baseurl + '/v1/profile';
-  describe(uri, function () {
-    it("use cookie to view profile", function (done) {
+  describe(uri, function() {
+    it("use cookie to view profile", function(done) {
       request({
         method: "GET",
         uri: uri,
@@ -37,28 +50,28 @@ describe("login endpoints >>", function () {
         headers: {
           Cookie: cookie
         }
-      }, function (error, response, body) {
+      }, function(error, response, body) {
         console.log(body);
         expect(error).to.be.equal(null);
         expect(response.statusCode).to.equal(200);
         var data = JSON.parse(body);
-        expect(data.username).to.be.equal('admin');
+        expect(data.username).to.be.equal('admin@foo.com');
         done();
       });
     });
   });
 
 
-  describe('/v1/login', function () {
-    it("fail to login as admin:admin2 bad password", function (done) {
+  describe('/v1/login', function() {
+    it("fail to login as admin@foo.com:admin2 bad password", function(done) {
       request({
         method: "POST",
         json: {
-          "username": "admin",
+          "username": "admin@foo.com",
           "password": "admin2"
         },
         uri: loginurl
-      }, function (error, response, body) {
+      }, function(error, response, body) {
         expect(error).to.be.equal(null);
         expect(response.statusCode).to.equal(401);
         expect(response.headers['content-type']).to.be.equal('application/json; charset=utf-8');
@@ -67,5 +80,83 @@ describe("login endpoints >>", function () {
     });
   });
 
-});
 
+  describe('password reset', function() {
+    describe('signup as russjohnson09@gmail.com', function() {
+      var requestBody = {
+        "username": "russjohnson09@gmail.com",
+        "password": "russjohnson09@gmail.com"
+      }
+      userrussjohnson09 = requestBody;
+
+      it("/v1/login POST", function(done) {
+        request({
+          method: "POST",
+          body: JSON.stringify(requestBody),
+          uri: baseurl + '/v1/login',
+          headers: {
+            'content-type': 'application/json',
+          }
+        }, function(error, response, body) {
+          expect(error).to.be.null;
+          logger.info(body);
+          body = JSON.parse(body);
+          logger.info(JSON.stringify(body, null, '    '));
+          logger.info(JSON.stringify(response.headers, null, '    '));
+          expect(response.statusCode).to.be.equal(201);
+
+          expect(response.headers['content-type']).to.be.equal('application/json; charset=utf-8');
+          expect(response.headers['set-cookie']).not.to.be.undefined;
+
+          expect(body.message).to.be.a('string');
+          expect(body.status).to.be.a('string');
+          expect(body.status).to.be.equal('success');
+
+
+          userrussjohnson09.cookie = response.headers['set-cookie'];
+
+          done();
+        });
+      });
+    });
+
+
+    describe('reset password', function() {
+
+      it("/v1/reqestpasswordreset POST", function(done) {
+        var requestBody = {
+          "username": "russjohnson09@gmail.com"
+        };
+        request({
+          method: "POST",
+          body: JSON.stringify(requestBody),
+          uri: baseurl + '/v1/reqestpasswordreset',
+          headers: {
+            'content-type': 'application/json',
+            // Cookie: cookie //no authentication required
+
+          }
+        }, function(error, response, body) {
+          expect(error).to.be.null;
+          logger.info(body);
+          body = JSON.parse(body);
+          logger.info(JSON.stringify(body, null, '    '));
+          logger.info(JSON.stringify(response.headers, null, '    '));
+          expect(response.statusCode).to.be.equal(201);
+
+          expect(response.headers['content-type']).to.be.equal('application/json; charset=utf-8');
+          expect(response.headers['set-cookie']).not.to.be.undefined;
+
+          expect(body.message).to.be.a('string');
+          expect(body.status).to.be.a('string');
+          expect(body.status).to.be.equal('success');
+
+          done();
+        });
+      })
+
+    })
+
+  })
+
+});
--- nodeserver/v1/login/main.js
+++ nodeserver/v1/login/main.js
@@ -1,14 +1,18 @@
-module.exports = function (opts) {
+module.exports = function(opts) {
     var http = require('http');
     var url = require('url');
     var express = require('express');
     var winston = opts.winston || require('winston');
     var fs = require('fs');
     var ObjectID = require('mongodb').ObjectID;
-    var uuid = require('node-uuid');
 
     var router = express.Router();
     var bcrypt = require('bcrypt');
+
+    const crypto = require('crypto');
+
+    const nodemailer = require('nodemailer');
+    const uuid = require('node-uuid');
 
 
     var database = opts.database;
@@ -18,46 +22,53 @@ module.exports = function (opts) {
 
     const LocalStrategy = require('passport-local').Strategy;
 
-    var comparePassword = function (password, hash, callback) {
+    var comparePassword = function(password, hash, callback) {
         // Load hash from your password DB. 
-        bcrypt.compare(password, hash, function (err, res) {
+        bcrypt.compare(password, hash, function(err, res) {
             callback(err, res)
         });
     }
 
-    var hashPassword = function (password, callback) {
-        bcrypt.genSalt(function (err, salt) {
-            bcrypt.hash(password, salt, function (err, hash) {
+    var hashPassword = function(password, callback) {
+        bcrypt.genSalt(function(err, salt) {
+            bcrypt.hash(password, salt, function(err, hash) {
                 callback(err, hash);
             });
         });
     }
 
     passport.use(new LocalStrategy(
-        function (username, password, done) {
+        function(username, password, done) {
             winston.debug('start authentication');
             winston.debug('start authentication');
             winston.debug(username);
             winston.debug(password);
-            User.findOne({ username: username }, function (err, user) {
+            User.findOne({
+                username: username
+            }, function(err, user) {
                 if (err) {
                     winston.error(err);
                     return done(err);
                 }
                 if (!user) {
-                    hashPassword(password, function (err, hash) {
+                    hashPassword(password, function(err, hash) {
                         if (err) {
                             winston.error(err);
                         }
-                        User.insertOne({ username: username, password: hash },
-                            function (error, result) {
+                        User.insertOne({
+                                username: username,
+                                password: hash
+                            },
+                            function(error, result) {
                                 winston.debug(result);
-                                return done(null, { _id: result.insertedId });
+                                return done(null, {
+                                    _id: result.insertedId
+                                });
                             });
                     });
                     return;
                 }
-                comparePassword(password, user.password, function (err, res) {
+                comparePassword(password, user.password, function(err, res) {
                     if (!res) {
                         winston.debug('password does not match')
                         return done(null, false);
@@ -74,16 +85,18 @@ module.exports = function (opts) {
     ));
 
 
-    passport.serializeUser(function (user, done) {
+    passport.serializeUser(function(user, done) {
         winston.debug('serializeUser');
         winston.debug(user);
         done(null, user._id);
     });
 
-    passport.deserializeUser(function (id, done) {
+    passport.deserializeUser(function(id, done) {
         winston.debug('deserializeUser');
         winston.debug(id);
-        User.findOne({ _id: ObjectID(id) }, function (err, user) {
+        User.findOne({
+            _id: ObjectID(id)
+        }, function(err, user) {
             winston.debug(user);
             if (err) {
                 winston.error(err);
@@ -92,16 +105,101 @@ module.exports = function (opts) {
         });
     });
 
+
+    function getToken() {
+        return crypto.randomBytes(64).toString('hex');
+    }
+
+    function doPasswordReset(username) {
+        var token = getToken();
+        
+        // winston.warn('password_reset');
+
+        User.findOne({
+            username: username
+        }, function(err, user) {
+            if (err) {
+                winston.error(err);
+                return;
+            }
+            if (!user) {
+                return;
+            }
+            var password_reset = {
+                token: token,
+                created: Date.now()
+            }
+            User.updateOne({
+                _id: user._id
+            }, {
+                $set: {
+                    'password_reset': password_reset
+                }
+            }, function(error, result) {});
+
+
+            var fs = require('fs');
+            if (fs.existsSync(__dirname + '/../params.js')) {
+                process.env = require(__dirname + '/../params.js').env;
+            }
+
+
+
+
+            if (process.env.SMTP_TRANSPORT) {
+                var transporter = nodemailer.createTransport(process.env.SMTP_TRANSPORT);
+            }
+
+            var mailOptions = {
+                from: '"Coderuss" <russ@coderuss.com>', // sender address
+                to: user.username, // list of receivers
+                subject: 'Password reset', // Subject line
+                text: 'Password reset' + JSON.stringify(password_reset), // plaintext body
+                html: 'Password reset' + JSON.stringify(password_reset),
+                headers: {
+                    "message-id": uuid.v1()
+                }
+            };
+
+            // send mail with defined transport object
+            transporter.sendMail(mailOptions, function(error, info) {
+                if (error) {
+                    return console.log(error);
+                }
+                console.log('Message sent: ' + info.response);
+            });
+            
+
+        });
+
+    }
+
+
+    router.post('/reqestpasswordreset', function(req, res) {
+
+        doPasswordReset(req.body.username);
+
+        res.status(201);
+        res.json({
+            status: 'success',
+            message: "Reset link sent to user\'s email"
+        }).end();
+    })
+
     //http://passportjs.org/docs/authenticate
-    router.post('/login', function (req, res, next) {
-        return passport.authenticate('local', function (err, user, info) {
-            if (err) { return next(err); }
+    router.post('/login', function(req, res, next) {
+        return passport.authenticate('local', function(err, user, info) {
+            if (err) {
+                return next(err);
+            }
 
             winston.debug(user);
 
             if (user !== false) {
-                req.logIn(user, function (err) {
-                    if (err) { return next(err); }
+                req.logIn(user, function(err) {
+                    if (err) {
+                        return next(err);
+                    }
                     res.setHeader('Content-Type', 'application/json; charset=utf-8');
                     res.status(201);
                     return res.send(JSON.stringify({
@@ -119,22 +217,26 @@ module.exports = function (opts) {
                     status: "unauthorized"
                 }));
             }
-            if (!user) { return res.redirect('/login'); }
-            req.logIn(user, function (err) {
-                if (err) { return next(err); }
+            if (!user) {
+                return res.redirect('/login');
+            }
+            req.logIn(user, function(err) {
+                if (err) {
+                    return next(err);
+                }
                 return res.redirect('/users/' + user.username);
             });
         })(req, res, next);
     });
 
     router.get('/logout',
-        function (req, res) {
+        function(req, res) {
             req.logout();
             res.redirect('/');
         });
 
     router.get('/profile',
-        function (req, res) {
+        function(req, res) {
             winston.debug(req.isAuthenticated());
             if (!req.isAuthenticated()) {
                 return res.redirect('/public/login');
@@ -148,8 +250,10 @@ module.exports = function (opts) {
 
     //authorize the application
     router.post('/oauth/authorize',
-        function (req, res) {
-            res.json({'message': 'hello from oauth'});
+        function(req, res) {
+            res.json({
+                'message': 'hello from oauth'
+            });
             winston.debug(req.isAuthenticated());
             if (!req.isAuthenticated()) {
                 return res.redirect('/public/login');
@@ -162,8 +266,10 @@ module.exports = function (opts) {
     //request access token
     //open to other domains
     router.post('/oauth/access_token',
-        function (req, res) {
-            res.json({'message': 'hello from oauth'});
+        function(req, res) {
+            res.json({
+                'message': 'hello from oauth'
+            });
             winston.debug(req.isAuthenticated());
             if (!req.isAuthenticated()) {
                 return res.redirect('/public/login');

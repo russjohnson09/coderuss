e180a20bd35b68018f87aa89bcc1e308420784d1
Russ Johnson <russjohnson09@gmail.com>
2017-03-29 15:37:53 +0000

    revert graylog

--- bin/graylog-start.sh
+++
@@ -1,6 +0,0 @@
- #!/usr/bin/env bash
-cd "$(dirname "$0")"
-cp ./conf/collector.conf /etc/graylog/collector/collector.conf -v
-sudo chmod 777 /etc/graylog/collector -R
-sudo stop graylog-collector
-sudo start graylog-collector
--- bin/java-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install openjdk-8-jre
-
-java --version

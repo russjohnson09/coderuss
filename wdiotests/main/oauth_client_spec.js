7ba369a7ab0247684c023111ef210805c26b71d8
Russ Johnson <russjohnson09@gmail.com>
2017-04-05 11:33:37 +0000

    create an oauthclient from wdio tests

--- wdiotests/main/oauth_client_spec.js
+++ wdiotests/main/oauth_client_spec.js
@@ -2,16 +2,21 @@ var expect = require('chai').expect;
 const path = require('path');
 
 var baseurl = "http://localhost:3000";
+const BASE_URL = "http://localhost:3000";
 
 const publicurl = baseurl;
 
+const SCREENSHOTS_DIR = __dirname + '/screenshots';
+
+console.log(SCREENSHOTS_DIR);
+
 describe(path.basename('oauth_client_spec'), function() {
     it('open login page', function() {
-        browser.url(publicurl+'/login');
+        browser.url(publicurl + '/login');
         // expect(browser.getUrl()).to.be.equal(publicurl+'/url'+1);
         browser.screenshot();
     })
-    it('should be able to login', function () {
+    it('should be able to login', function() {
         // filtering property commands
         $('input[name=\'username\']').setValue('admin123456');
         $('input[name=\'password\']').setValue('admin123456');
@@ -21,10 +26,26 @@ describe(path.basename('oauth_client_spec'), function() {
 
         browser.waitForVisible('form#create-todo');
 
-        expect(browser.isVisible('form#create-todo'),'todo form is visible').to.be.true;
+        expect(browser.isVisible('form#create-todo'), 'todo form is visible').to.be.true;
     });
-    
-    describe('go to oauthclient page', function() {
 
+    describe('create an oauthclient', function() {
+        describe('go to oauthclient page', function() {
+            it('browser url', function() {
+                browser.url(BASE_URL + '/oauthclient');
+            });
+            it('/oauthclient validate', function() {
+                expect(browser.isVisible('.coderuss-oauthclient-submit')).to.be.true;
+            });
+        });
+
+        describe('submit oauthclient', function() {
+            it('/oauthclient validate', function() {
+                expect(browser.isVisible('.coderuss-oauthclient-submit')).to.be.true;
+                browser.click('.coderuss-oauthclient-submit');
+                console.log(SCREENSHOTS_DIR);
+                browser.saveScreenshot(SCREENSHOTS_DIR+'/oauth-client-submit.png');
+            });
+        })
     });
-});
+})

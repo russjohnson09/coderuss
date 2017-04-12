d5b87424bada19a5ae1702e15f709c8b91bfa5f7
Russ Johnson <russjohnson09@gmail.com>
2017-04-12 11:35:48 +0000

    fixed cdn added oauth to changelog

--- nodeserver/public/login/login.js
+++ nodeserver/public/login/login.js
@@ -2,72 +2,64 @@ var app = angular.module('login', []);
 
 app.config(['$locationProvider', function($locationProvider) {
 	$locationProvider.html5Mode({
-		enabled: true
-			// ,requireBase: false
+		enabled: true,
+		requireBase: false
 	});
 }]);
 
-app.controller('mainController',function($rootScope, $scope, $location, $http) {
+app.controller('mainController', function($rootScope, $scope, $location, $http) {
 	$scope.formData = {};
-	
-	$scope.alerts = [];
-	
-	var params = $location.search();
-	
-	console.log(params);
-	
-	 // $scope.addAlert = function(msg) {
-  //};
 
-  $scope.closeAlert = function(index) {
-    $scope.alerts.splice(index, 1);
-  };
-	
-	
+	$scope.alerts = [];
+
+	var params = $location.search();
+
+	$scope.closeAlert = function(index) {
+		$scope.alerts.splice(index, 1);
+	};
+
+
 	$scope.reqestpasswordreset = function() {
 		console.log($scope.formData);
 		$http.post('/v1/reqestpasswordreset', $scope.formData).then(function successCallback(res) {
-			console.log('success');
-			console.log(res);
-			console.log(res.status);
-			console.log(res.data);
-			if (res.status === 201) {
-				    $scope.alerts.push({msg: 'Email sent with password reset link!', show: true});
+				console.log('success');
+				console.log(res);
+				console.log(res.status);
+				console.log(res.data);
+				if (res.status === 201) {
+					$scope.alerts.push({
+						msg: 'Email sent with password reset link!',
+						show: true
+					});
 
-				// $scope.message = ;
-			}
-		},
-		function errorCallback(res) {
-			console.log(res.status);
-			
-			
-		});
+				}
+			},
+			function errorCallback(res) {
+				console.log(res.status);
+			});
 	}
 
-	
-	// reqestpasswordreset
 
 	$scope.login = function() {
 		console.log($scope.formData);
 		$http.post('/v1/login', $scope.formData).then(function successCallback(res) {
-			console.log('success');
-			console.log(res);
-			console.log(res.status);
-			console.log(res.data);
-			if (res.status === 201) {
-				if (!params.redirect_uri) {
-					params.redirect_uri = '/v1/todos/public/';
+				console.log('success');
+				console.log(res);
+				console.log(res.status);
+				console.log(res.data);
+				if (res.status === 201) {
+					if (!params.redirect_uri) {
+						params.redirect_uri = '/v1/todos/public/';
+					}
+					console.log('redirect to ' + params.redirect_uri)
+					window.location = params.redirect_uri;
 				}
-				console.log('redirect to '+params.redirect_uri)
-				window.location = params.redirect_uri;
-			}
-		},
-		function errorCallback(res) {
-			console.log(res.status);
-			
-			
-		});
+			},
+			function errorCallback(res) {
+				console.log(res.status);
+
+
+			});
 	}
 
-}
-);
+});

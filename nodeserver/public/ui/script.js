885d2cdfae071bd31ffe6c5d7e608572aff25d09
Russ Johnson <russ@zeamster.com>
2017-04-25 18:14:07 +0000

    added bootstrap

--- nodeserver/public/ui/script.js
+++ nodeserver/public/ui/script.js
@@ -1,53 +1,74 @@
 // script.js
 
-    // create the module and name it scotchApp
-        // also include ngRoute for all our routing needs
-    var scotchApp = angular.module('scotchApp', ['ngRoute']);
+// create the module and name it scotchApp
+// also include ngRoute for all our routing needs
+var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
-    // configure our routes
-    scotchApp.config(function($routeProvider) {
-        $routeProvider
 
-            // route for the home page
-            .when('/', {
-                templateUrl : 'pages/home.html',
-                controller  : 'mainController'
-            })
+//http://stackoverflow.com/questions/16677528/location-switching-between-html5-and-hashbang-mode-link-rewriting
+// scotchApp.config(['$locationProvider', function($locationProvider) {
+//     $locationProvider.html5Mode({
+//         enabled: true,
+//         requireBase: false,
+//     }).hashPrefix('!');
+//     // $locationProvider.hashPrefix('');
+// }]);
 
-            // route for the about page
-            .when('/about', {
-                templateUrl : 'pages/about.html',
-                controller  : 'aboutController'
-            })
+// app.controller('mainController', function($rootScope, $scope, $location, $http) {
 
-            // route for the contact page
-            .when('/contact', {
-                templateUrl : 'pages/contact.html',
-                controller  : 'contactController'
-            })
-            
-            .when('/todos', {
-                templateUrl : 'pages/todos.html',
-                controller  : 'todosController'
-            })
-            
-            .otherwise({templateUrl:'pages/404.html'}); // Render 404 view
+// configure our routes
+scotchApp.config(function($routeProvider) {
+    console.log($routeProvider);
+    $routeProvider
 
-    });
-    
-    scotchApp.controller('todosController', function($scope) {
-    });
+    // route for the home page
+        .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'mainController'
+    })
 
-    // create the controller and inject Angular's $scope
-    scotchApp.controller('mainController', function($scope) {
-        // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
-    });
+    // route for the about page
+    .when('/about', {
+        templateUrl: 'pages/about.html',
+        controller: 'aboutController'
+    })
 
-    scotchApp.controller('aboutController', function($scope) {
-        $scope.message = 'Look! I am an about page.';
-    });
+    // route for the contact page
+    .when('/contact', {
+        templateUrl: 'pages/contact.html',
+        controller: 'contactController'
+    })
 
-    scotchApp.controller('contactController', function($scope) {
-        $scope.message = 'Contact us! JK. This is just a demo.';
-    });
+    .when('/todos', {
+        templateUrl: 'pages/todos.html',
+        controller: 'todosController'
+    }) 
+    .when('/bootstrap', {
+        templateUrl: 'pages/bootstrap.html',
+        controller: 'todosController'
+    }) 
+
+    .otherwise({
+        templateUrl: 'pages/404.html'
+    }); // Render 404 view
+
+});
+
+scotchApp.controller('todosController', function($rootScope, $scope, $location, $http) {
+    console.log($location);
+    console.log($location.search());
+});
+
+// create the controller and inject Angular's $scope
+scotchApp.controller('mainController', function($scope) {
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+scotchApp.controller('aboutController', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+scotchApp.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});

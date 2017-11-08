ff8bb49f5ac918238eb98feec752efea6cc919c9
russjohnson09@gmail.com <russjohnson09@gmail.com>
2017-11-08 08:34:35 -0500

    travis run added timeout

--- nodeserver/tests/travis_run.js
+++ nodeserver/tests/travis_run.js
@@ -12,22 +12,23 @@ require('dotenv').config();
 
 var main = require(__dirname + './../main.js')({}, function(port) {
     console.log('server listening on port ' + port);
-    var child = spawn("npm",['run','test:main'],{ cwd: __dirname, env: process.env });
 
-    child.stdout.on('data', function(data) {
-        process.stdout.write(data);
-    });
+    setTimeout(function() {
+        var child = spawn("npm",['run','test:main'],{ cwd: __dirname, env: process.env });
 
-    child.stderr.on('data', function(data) {
-        process.stderr.write(data);
-        // process.exit(1);
-    });
-    child.on('exit', function(exitcode) {
-        process.exit(exitcode);
-        if (exitcode) {
-            
-        }
-    })
+        child.stdout.on('data', function(data) {
+            process.stdout.write(data);
+        });
 
+        child.stderr.on('data', function(data) {
+            process.stderr.write(data);
+            // process.exit(1);
+        });
+        child.on('exit', function(exitcode) {
+            process.exit(exitcode);
+            if (exitcode) {
 
+            }
+        })
+    },10000);
 });

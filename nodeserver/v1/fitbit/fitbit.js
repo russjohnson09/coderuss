f7f1ea912059e535308a0998fa420f2fc4a608bd
russjohnson09@gmail.com <russjohnson09@gmail.com>
2017-11-13 22:22:16 -0500

    added to fitbit endpoint

--- nodeserver/v1/fitbit/fitbit.js
+++ nodeserver/v1/fitbit/fitbit.js
@@ -1,19 +1,17 @@
- console.log('loaded fitbit.js',Date.now());
+console.log('loaded fitbit.js', Date.now());
 
- /**
-  *
-  * https://github.com/jaredhanson/passport-fitbit
-  *
-  * @param opts
-  * @returns router
-  */
- module.exports = function(opts)
-{
-
+/**
+ *
+ * https://github.com/jaredhanson/passport-fitbit
+ *
+ * @param opts
+ * @returns router
+ */
+module.exports = function (opts) {
     let passport = require('passport');
     let request = require('request');
-    var FitbitStrategy = require( 'passport-fitbit-oauth2' ).FitbitOAuth2Strategy;;
-
+    var FitbitStrategy = require('passport-fitbit-oauth2').FitbitOAuth2Strategy;
+    ;
 
     let winston = opts.winston || require('winston');
     winston.info('loaded winston');
@@ -30,126 +28,85 @@
 
     winston.info('hello fitbit.js');
 
-    function addUpdateUser(opts,cb) {
+    function addUpdateUser(opts, cb) {
         winston.info('addUpdateUser' + JSON.stringify(opts));
-
-        cb(null,{});
+        cb(null, {});
     }
 
     passport.use(new FitbitStrategy({
-            clientID:     FITBIT_CLIENT_ID,
+            clientID: FITBIT_CLIENT_ID,
             clientSecret: FITBIT_CLIENT_SECRET,
-            // consumerKey: FITBIT_CONSUMER_KEY,
-            // consumerSecret: FITBIT_CONSUMER_SECRET,
             callbackURL: callbackUrl,
-            // callbackURL: "http://127.0.0.1:3000/auth/fitbit/callback"
-        },
-        function(token, tokenSecret, profile, cb) {
-            addUpdateUser({ fitbitId: profile.id, token : token,
-                tokenSecret: tokenSecret}, function(err,user) {
-                return cb(err, user);
-            });
-            // User.findOrCreate({ fitbitId: profile.id }, function (err, user) {
-            //     return cb(err, user);
-            // });
-        }
+        },function(){}
     ));
 
+    router.use(function (req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        else {
+            return res.status(401).json({message: 'must be logged in'});
+        }
+    });
 
 
-
-    router.get('/',function(req,res,next) {
+    router.get('/', function (req, res, next) {
         return res.json({})
     });
 
     router.get('/auth',
-        passport.authenticate('fitbit', { scope:
-            ['activity','heartrate','location','profile'] }
+        passport.authenticate('fitbit', {
+                scope: ['activity', 'heartrate', 'location', 'profile']
+            }
         ));
 
     const FITBIT_OAUTH_URL = 'https://www.fitbit.com/oauth2/authorize';
-
     const FITBIT_ACCESS_TOKEN_URL = 'https://api.fitbit.com/oauth2/token';
 
+    router.get('/auth/callback', function (req, res, next) {
+            winston.info('/auth/callback');
+            let code = req.query.code;
+            let requestObj = {
+                grant_type: 'authorization_code',
+                code: code,
+                redirect_uri: callbackUrl
+            };
+            request({
+                method: 'POST',
+                auth: {
+                    user: FITBIT_CLIENT_ID,
+                    password: FITBIT_CLIENT_SECRET
+                },
+                url: FITBIT_ACCESS_TOKEN_URL,
+                form: requestObj,
+                // body: JSON.stringify(requestObj)
+            }, function (error, res, body) {
+                if (error) {
+                    winston.error(error);
+                }
+                winston.info(res.headers);
+                winston.info(res.statusCode);
 
-    // const GITHUB_OAUTH_URL = 'https://github.com/login/oauth/authorize';
-    // const GITHUB_ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token';
-    // const GITHUB_API_URL = 'https://api.github.com';
+                if (res.statusCode !== 200) {
+                    winston.error('unexected statusCode ' + res.statusCode);
+                    winston.error(body);
 
-    router.get('/auth/callback',function(req,res,next) {
-
-        return passport.authenticate('fitbit'
-            , function(err, user, info) {
-                console.log('/auth/callback',err,user,info);
-
-                if (err) {
-                    winston.error('bad fitbit auth')
-                    return res.redirect('/err');
                 }
                 else {
-                    return res.redirect('/test')
-                }
-                // Generate a JSON response reflecting authentication status
-                if (! user) {
+                    winston.info(body);
 
-                    return res.send({ success : false, message : 'authentication failed' });
-                }
-            }
-        )(req, res, next);
-
-        console.log('/auth/callback')
-        winston.info('/auth/callback');
-
-        let code = req.query.code;
-
-        let requestObj = {
-            grant_type: 'authorization_code',
-            code: code,
-            redirect_uri: callbackUrl
-        };
-
-        winston.info(JSON.stringify(requestObj));
-
-        //client_id:client secret
-        request({
-            method: 'POST',
-            // headers: {
-            //     'Content-Type': 'application/json'
-            // },
-            auth: {
-                user: FITBIT_CLIENT_ID,
-                password: FITBIT_CLIENT_SECRET
-            },
-            url: FITBIT_ACCESS_TOKEN_URL,
-            form: requestObj,
-            // body: JSON.stringify(requestObj)
-        }, function (error, res, body) {
-            if (error) {
-                winston.error(error);
-            }
-            winston.info(res.headers);
-            winston.info(res.statusCode);
-
-            if (res.statusCode !== 200) {
-                winston.error('unexected statusCode ' + res.statusCode);
-                winston.error(body);
-
-            }
-            else {
-                winston.info(body);
-
-                let access_token_response = JSON.parse(body);
-                let access_token = access_token_response.access_token;
-                let user_id = access_token_response.user_id;
-                let refresh_token = access_token_response.refresh_token;
-                let scope = access_token_response.scope;
+                    let access_token_response = JSON.parse(body);
+                    let access_token = access_token_response.access_token;
+                    let user_id = access_token_response.user_id;
+                    let refresh_token = access_token_response.refresh_token;
+                    let scope = access_token_response.scope;
 
 
-                //GET https://api.fitbit.com/1/user/-/profile.json
+                    //GET https://api.fitbit.com/1/user/-/profile.json
 
-                /**
-                 * https://dev.fitbit.com/reference/web-api/oauth2/#making-requests
-                 * {
+                    /**
+                     * https://dev.fitbit.com/reference/web-api/oauth2/#making-requests
+                     * {
     "errors": [
         {
             "errorType": "expired_token",
@@ -157,52 +114,23 @@
         }
     ]
 }
-                 */
-                //GET https://api.fitbit.com/1/user/[user-id]/profile.json
-                //Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0MzAzNDM3MzUsInNjb3BlcyI6Indwcm8gd2xvYyB3bnV0IHdzbGUgd3NldCB3aHIgd3dlaSB3YWN0IHdzb2MiLCJzdWIiOiJBQkNERUYiLCJhdWQiOiJJSktMTU4iLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJpYXQiOjE0MzAzNDAxMzV9.z0VHrIEzjsBnjiNMBey6wtu26yHTnSWz_qlqoEpUlpc
+                     */
+                    //GET https://api.fitbit.com/1/user/[user-id]/profile.json
+                    //Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0MzAzNDM3MzUsInNjb3BlcyI6Indwcm8gd2xvYyB3bnV0IHdzbGUgd3NldCB3aHIgd3dlaSB3YWN0IHdzb2MiLCJzdWIiOiJBQkNERUYiLCJhdWQiOiJJSktMTU4iLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJpYXQiOjE0MzAzNDAxMzV9.z0VHrIEzjsBnjiNMBey6wtu26yHTnSWz_qlqoEpUlpc
+                    request({
+                        method: 'GET',
+                        headers: {
+                            'Authorization': 'Bearer ' + access_token
+                        },
+                        url: 'https://api.fitbit.com/1/user/-/profile.json',
+                        form: requestObj,
+                        // body: JSON.stringify(requestObj)
+                    }, function (error, res, body) {
 
-
-                request({
-                    method: 'GET',
-                    headers: {
-                        'Authorization': 'Bearer ' + access_token
-                    },
-                    url: 'https://api.fitbit.com/1/user/-/profile.json',
-                    form: requestObj,
-                    // body: JSON.stringify(requestObj)
-                }, function (error, res, body) {
-
-                    winston.info(error,body,res.headers);
-                })
-            }
-        })
-
-
-        },function(req,res,next) {
-
-        return;
-        console.log('/auth/callback');
-
-        passport.authenticate('fitbit'
-            , function(err, user, info) {
-                console.log('/auth/callback',err,user,info);
-
-                if (err) {
-                    return next(err); // will generate a 500 error
+                        winston.info(error, body, res.headers);
+                    })
                 }
-                // Generate a JSON response reflecting authentication status
-                if (! user) {
-                    return res.send({ success : false, message : 'authentication failed' });
-                }
-            }
-        )(req, res, next);
-            // { failureRedirect: '/login' }),
-        // function(req, res) {
-        //     // Successful authentication, redirect home.
-        //     res.redirect('/');
-        //
-
-
+            })
         }
     );
 
@@ -211,77 +139,104 @@
 };
 
 
- if (require.main === module) {
-     (function() {
-         let express = require('express');
-         let app = express();
-         let winston = require('winston');
-         require('dotenv').config();
+if (require.main === module) {
+    (function () {
+        let express = require('express');
+        let app = express();
+        let winston = require('winston');
+        let passport = require('passport');
+
+        require('dotenv').config();
+
+        const MONGO_URI = process.env.MONGO_URI;
 
 
-         // var mainLogger = new winston.Logger({
-         //     exceptionHandlers: [new winston.transports.Console({
-         //         colorize: true,
-         //         json: true
-         //     })],
-         //     exitOnError: process.env.NODE_ENV == 'DEV'
-         // });
-
-         const MONGO_URI = process.env.MONGO_URI;
-         const MongoClient = require('mongodb').MongoClient;
-         let passport = require('passport');
-
-         MongoClient.connect(MONGO_URI, function(err, db) {
-             if (err) {
-                 throw err;
-             }
-             app.listen(3000, function () {
-                 let router = module.exports({
-                     FITBIT_CLIENT_ID: process.env.FITBIT_CLIENT_ID,
-                     FITBIT_CLIENT_SECRET: process.env.FITBIT_CLIENT_SECRET,
-                     BASE_URL: 'http://localhost:3000/v1/fitbit',
-                     winston: winston
-                 });
-                 app.use('/v1/fitbit', function(req,res,next) {
-                     winston.info('user');
-                     winston.info(req.user);
-
-                     next();
-                 } ,router);
-
-                 app.use('/v1', require(__dirname + '/../../v1/login/main.js')({
-                     winston: winston,
-                     database: db,
-                     passport: passport,
-                 }).router);
-
-                 let cp = require('child_process');
-                 let spawn = cp.spawn;
-
-                 let child = spawn("mocha", ['./**/*_spec.js'],
-                     {cwd: __dirname, env: process.env});
-                 child.stdout.on('data', function(data) {
-                     process.stdout.write(data);
-                 });
-
-                 child.stderr.on('data', function(data) {
-                     process.stderr.write(data);
-                 });
-                 child.on('exit', function(exitcode) {
-                     if (exitcode !== 0) {
-                         process.exit(exitcode);
-                     }
-                 });
-
-             });
-
-         });
+        var bodyParser = require('body-parser');
+        app.use(bodyParser.urlencoded({
+            extended: true
+        }));
+        app.use(bodyParser.json());
+        const cookieParser  = require('cookie-parser');
+        app.use(cookieParser());
+        var expressSession = require('express-session');
+        var sessionMiddleware = expressSession({
+            secret: process.env.EXPRESS_SESSION_SECRET,
+            store: new(require("connect-mongo")(expressSession))({
+                url: MONGO_URI
+            })
+        });
+        app.use(sessionMiddleware);
+        app.use(passport.initialize());
+        app.use(passport.session());
 
 
-     })();
+        var mainLogger = new winston.Logger({
+            transports: [
+                new winston.transports.Console({ level: 'debug' }),
+                ],
+            exceptionHandlers: [new winston.transports.Console({
+                colorize: true,
+                json: true
+            })],
+            exitOnError: process.env.NODE_ENV == 'DEV'
+        });
+
+        mainLogger.debug('mainLogger');
+        mainLogger.info('mainLogger');
+        mainLogger.error('mainLogger');
+
+        const MongoClient = require('mongodb').MongoClient;
+
+        MongoClient.connect(MONGO_URI, function (err, db) {
+            if (err) {
+                throw err;
+            }
+            app.listen(3000, function () {
+                let router = module.exports({
+                    FITBIT_CLIENT_ID: process.env.FITBIT_CLIENT_ID,
+                    FITBIT_CLIENT_SECRET: process.env.FITBIT_CLIENT_SECRET,
+                    BASE_URL: 'http://localhost:3000/v1/fitbit',
+                    winston: winston
+                });
+
+                app.use('/v1', require(__dirname + '/../../v1/login/main.js')({
+                    winston: mainLogger,
+                    database: db,
+                    passport: passport,
+                }).router);
+
+                app.use('/v1/fitbit', function (req, res, next) {
+                    winston.info('user');
+                    winston.info(req.user);
+                    next();
+                }, router);
+
+                let cp = require('child_process');
+                let spawn = cp.spawn;
+
+                let child = spawn("mocha", ['./**/*_spec.js'],
+                    {cwd: __dirname, env: process.env});
+                child.stdout.on('data', function (data) {
+                    process.stdout.write(data);
+                });
+
+                child.stderr.on('data', function (data) {
+                    process.stderr.write(data);
+                });
+                child.on('exit', function (exitcode) {
+                    if (exitcode !== 0) {
+                        // process.exit(exitcode);
+                    }
+                });
+
+            });
+
+        });
 
 
+    })();
 
- } else {
-     console.log('required as a module');
- }
+
+} else {
+    console.log('required as a module');
+}
--- nodeserver/v1/fitbit/tests/fitbit_spec.js
+++ nodeserver/v1/fitbit/tests/fitbit_spec.js
@@ -13,13 +13,37 @@ describe(path.basename(__dirname), function () {
 
     let headers = {};
 
+    describe('/v1/login', function() {
+        it("successfully login with admin@foo.com:admin@foo.com", function(done) {
+            username = 'admin@foo.com';
+            request({
+                method: "POST",
+                json: {
+                    "username": username,
+                    "password": "admin@foo.com"
+                },
+                uri: BASE_URL + '/v1/login'
+            }, function(error, response, body) {
+                console.log(body);
+                expect(error).to.be.equal(null);
+                expect(response.statusCode).to.equal(201);
+                expect(response.headers['content-type']).to.be.equal('application/json; charset=utf-8');
+                expect(response.headers['set-cookie']).not.to.be.undefined;
+                cookie = response.headers['set-cookie'];
+                done();
+            });
+        });
+    });
 
     describe('fitbit', function () {
         describe('/v1/fitbit as admin', function () {
             it("successfully login", function (done) {
                 request({
                     method: "GET",
-                    uri: BASE_URL + '/v1/fitbit'
+                    uri: BASE_URL + '/v1/fitbit',
+                    headers: {
+                        Cookie: cookie
+                    }
                 }, function (error, response, body) {
                     expect(error).to.be.equal(null);
                     expect(response.statusCode).to.equal(200);

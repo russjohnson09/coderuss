9de9b160ff59c69121135d12aa35051a998837c5
Russell Johnson <RussJ@DetroitSoftware.com>
2018-01-22 17:05:04 -0500

    added misc

--- nodeserver/v1/misc/misc.js
+++ nodeserver/v1/misc/misc.js
@@ -727,6 +727,7 @@ if (require.main === module) {
         require('dotenv').config();
 
         const MONGO_URI = process.env.MONGO_URI;
+        console.log('MONGO_URI',MONGO_URI);
 
 
         var bodyParser = require('body-parser');
